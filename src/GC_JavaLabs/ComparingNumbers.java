15692a0d115a89af72427a6c8d5831659255511d
Louis Privette <louis.privette@gmail.com>
2018-07-18 06:56:55 -0400

    Commit Initialized. Lab Number 3 - Completed.

---
+++ src/GC_JavaLabs/ComparingNumbers.java
@@ -0,0 +1,49 @@
+package GC_JavaLabs;
+
+import java.util.Scanner;
+
+public class ComparingNumbers {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		int userNum;
+	
+		System.out.println("Enter a postive whole number between 1 and 100. :");
+		userNum = input.nextInt();
+		
+		if (userNum >= 1 && userNum <= 100) {
+			System.out.println("Your chosen number is " + userNum);
+		}
+			
+		else {
+			System.out.println(userNum + " is not a valid number.");
+			System.exit(0);
+		}
+		
+		if (userNum % 2 != 0) {
+			System.out.println("Your number of " + userNum + " is an odd number.");
+		}
+
+		if (userNum % 2 != 0 && userNum >= 60) {
+				System.out.println("Your number of " + userNum + " is an odd number and greater then 60.");
+			}
+		
+		if (userNum % 2 == 0) {
+			System.out.println("Your number of " + userNum + " is an even number.");
+		}
+		
+		if (userNum % 2 == 0 && userNum <= 25) {
+			System.out.println("Your number of " + userNum + " is an even number and less then 25.");
+		}
+		
+		if (userNum % 2 == 0 && userNum >= 60) {
+			System.out.println("Your number of " + userNum + " is an even number and greater then 60.");
+		}
+		
+		System.exit(0);
+
+		input.close(); // Stops Eclipse' "Resource leak" warning when using scanner.
+		}
+	}
+	
